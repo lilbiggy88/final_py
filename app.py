5e438588eb23993011ae619f1bda2a06648dace7
John Biggerstaff <john.r.biggerstaff@gmail.com>
2023-06-26 01:46:59 -0600

    Just some CSS left

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
 from flask_wtf import FlaskForm
@@ -14,21 +14,9 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
-
-class FormData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(10))
-    text = db.Column(db.String(100))
-
-db.create_all()
-
-
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -41,24 +29,30 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(80), nullable=False)
 
 
+class FormData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    time = db.Column(db.String(10))
+    text = db.Column(db.String(100))
+
+db.create_all()
+
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Sign Up!")
 
-def validate_username(self, username):
-    existing_username = User.query.filter_by(username=username.data).first()
-
-    if existing_username:
-        raise ValidationError("This username is already in use! Please choose a different username.")
+    def validate_username(self, username):
+        existing_username = User.query.filter_by(username=username.data).first()
+        if existing_username:
+            raise ValidationError("This username is already in use! Please choose a different username.")
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Login!")
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
@@ -71,39 +65,85 @@ def login():
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
                 return redirect(url_for('dashboard'))
-            
     return render_template('login.html', form=form)
 
 
-@app.route('/dashboard', methods=['GET', 'POST'] )
+@app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+    if request.method == 'POST':
+        item_id = request.form.get('item_id')
+        item = FormData.query.get(item_id)
 
-   data = FormData.query.all()
-   return render_template('dashboard.html', data=data)
+        if item:
+            db.session.delete(item)
+            db.session.commit()
 
-def get_time_options():
-    times = []
-    start_hour = 0
-    end_hour = 23
-    interval = 15
+    data = FormData.query.all()
+    return render_template('dashboard.html', data=data)
 
-    for hour in range(start_hour, end_hour + 1):
-        for minute in range(0, 60, interval):
-            formatted_hour = str(hour).zfill(2)
-            formatted_minute = str(minute).zfill(2)
-            time = f'{formatted_hour}:{formatted_minute}'
-            times.append(time)
+@app.route('/scheduler', methods=['GET', 'POST'])
+@login_required
+def scheduler():
 
-    return times
-    return render_template('dashboard.html')
+    def get_time_options():
+        times = [
+            '12:00 AM', '12:15 AM', '12:30 AM', '12:45 AM', '1:00 AM', '1:15 AM', '1:30 AM', '1:45 AM',
+            '2:00 AM', '2:15 AM', '2:30 AM', '2:45 AM', '3:00 AM', '3:15 AM', '3:30 AM', '3:45 AM',
+            '4:00 AM', '4:15 AM', '4:30 AM', '4:45 AM', '5:00 AM', '5:15 AM', '5:30 AM', '5:45 AM',
+            '6:00 AM', '6:15 AM', '6:30 AM', '6:45 AM', '7:00 AM', '7:15 AM', '7:30 AM', '7:45 AM',
+            '8:00 AM', '8:15 AM', '8:30 AM', '8:45 AM', '9:00 AM', '9:15 AM', '9:30 AM', '9:45 AM',
+            '10:00 AM', '10:15 AM', '10:30 AM', '10:45 AM', '11:00 AM', '11:15 AM', '11:30 AM', '11:45 AM',
+            '12:00 PM', '12:15 PM', '12:30 PM', '12:45 PM', '01:00 PM', '01:15 PM', '01:30 PM', '01:45 PM',
+            '2:00 PM', '2:15 PM', '2:30 PM', '2:45 PM', '3:00 PM', '3:15 PM', '3:30 PM', '3:45 PM',
+            '4:00 PM', '4:15 PM', '4:30 PM', '4:45 PM', '5:00 PM', '5:15 PM', '5:30 PM', '5:45 PM',
+            '6:00 PM', '6:15 PM', '6:30 PM', '6:45 PM', '7:00 PM', '7:15 PM', '7:30 PM', '7:45 PM',
+            '8:00 PM', '8:15 PM', '8:30 PM', '8:45 PM', '9:00 PM', '9:15 PM', '9:30 PM', '9:45 PM',
+            '10:00 PM', '10:15 PM', '10:30 PM', '10:45 PM', '11:00 PM', '11:15 PM', '11:30 PM', '11:45 PM'
+        ]
+        start_hour = 0
+        end_hour = 23
+        interval = 15
+
+        for hour in range(start_hour, end_hour + 1):
+            for minute in range(0, 60, interval):
+                formatted_hour = str(hour).zfill(2)
+                formatted_minute = str(minute).zfill(2)
+                time = f'{formatted_hour}:{formatted_minute}'
+                times.append(time)
+
+        return times
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        times = request.form.getlist('time[]')
+        texts = request.form.getlist('text[]')
+
+        for i in range(len(times)):
+            form_data = FormData(title=title if i == 0 else "", time=times[i], text=texts[i])
+            db.session.add(form_data)
+            db.session.commit()
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('scheduler.html', times=get_time_options())
+
+
+@app.route('/delete/<int:item_id>', methods=['POST'])
+@login_required
+def delete_entries(item_id):
+    item = FormData.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    flash('Entry deleted successfully.', 'success')
+    return redirect(url_for('dashboard'))
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -117,26 +157,6 @@ def signup():
         return redirect(url_for('login'))
 
     return render_template('signup.html', form=form)
-
-
-@app.route('/scheduler', methods=['GET', 'POST'])
-@login_required
-def scheduler():
-
-    if request.method == 'POST':
-        times = request.form.getlist('time[]')
-        texts = request.form.getlist('text[]')
-
-        # Store submitted data in the database
-        for i in range(len(times)):
-            form_data = FormData(time=times[i], text=texts[i])
-            db.session.add(form_data)
-        db.session.commit()
-
-        return redirect('/dashboard')
-
-    return render_template('scheduler.html', times=get_time_options())
-
 
 if __name__ == '__main__':
     app.run(debug=True)
